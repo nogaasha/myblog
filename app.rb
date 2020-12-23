41cc397cb6613e25e03e5d04960f7567e733c4a1
Nogaasha <sivosha2010@gmail.com>
2020-12-23 18:56:15 +0300

    add details.erb

--- app.rb
+++ app.rb
@@ -46,3 +46,13 @@ post '/new' do
 
 	redirect to '/'		
 end
+
+get '/details/:post_id' do
+	post_id= params[:post_id]
+	#Выбираем список постов с определенным id
+  results = @db.execute 'select * from Posts where id = ?', [post_id]
+  #выбираем этот пост в переменную row
+	@row = results[0]
+	
+	erb :details 
+end	
